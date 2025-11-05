5a27932fad3d3c17f56437a23ceac53513f77bc5
Marc T <marc.tremblay1995@gmail.com>
2025-11-05 15:29:58 -0500

    basic player movement

---
+++ Assets/_Project/Scripts/Input/InputReader.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.Events;
+using static PlayerInputActions;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace UnityDemoA
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "UnityDemoA/InputReader")]
+    public class InputReader : ScriptableObject, IPlayerActions
+    {
+        public event UnityAction<Vector2> Move = delegate { };
+        public event UnityAction<Vector2, bool> Look = delegate { };
+        
+        private PlayerInputActions _inputActions;
+        
+        public Vector2 Direction => _inputActions.Player.Move.ReadValue<Vector2>();
+
+        private void OnEnable()
+        {
+            if (_inputActions == null)
+            {
+                _inputActions = new PlayerInputActions();
+                _inputActions.Player.SetCallbacks(this);
+            }
+        }
+        
+        public void EnableInputActions() => _inputActions.Enable();
+        public void DisableInputActions() => _inputActions.Disable();
+
+        public void OnMove(CallbackContext context)
+        {
+            Move.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnLook(CallbackContext context)
+        {
+            Look.Invoke(context.ReadValue<Vector2>(), IsDeviceMouse(context));
+        }
+
+        private bool IsDeviceMouse(CallbackContext context) => context.control.device.name == "Mouse";
+
+        public void OnAttack(CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnInteract(CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnCrouch(CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnJump(CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnPrevious(CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnNext(CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnSprint(CallbackContext context)
+        {
+            // noop
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using KBCore.Refs;
+using UnityEngine;
+
+namespace UnityDemoA
+{
+    public class PlayerController : ValidatedMonoBehaviour
+    {
+        [Header( "References" )]
+        [SerializeField, Self] private Rigidbody rb;
+        [SerializeField] private Transform avatar;
+        [SerializeField, Anywhere] private InputReader input;
+        
+        [Header( "Movement Settings")]
+        [SerializeField] private float moveSpeed = 6f;
+        [SerializeField] private float turnSpeed = 10f;
+        [SerializeField] private float smoothTime = 0.2f;
+
+        private const float ZeroF = 0f;
+        private Transform mainCamera;
+        
+        private float currentSpeed;
+        private float velocity;
+
+        private void Awake()
+        {
+            mainCamera = Camera.main.transform;
+        }
+
+        private void Start() => input.EnableInputActions();
+
+        private void FixedUpdate()
+        {
+            HandleMovement();
+        }
+
+        private void HandleMovement()
+        {
+            var moveDirection = new Vector3(input.Direction.x, 0, input.Direction.y);
+            var adjustedDirection = Quaternion.AngleAxis(mainCamera.eulerAngles.y, Vector3.up) * moveDirection;
+
+            
+            if (adjustedDirection.magnitude > ZeroF)
+            {
+                HandleRotation(adjustedDirection);
+                HandleHorizontalMovement(adjustedDirection);
+                SmoothSpeed(adjustedDirection.magnitude);
+            }
+            else
+            {
+                SmoothSpeed(ZeroF);
+                
+                rb.linearVelocity = new Vector3(ZeroF, rb.linearVelocity.y, ZeroF);
+            }
+        }
+
+        private void HandleRotation(Vector3 adjustedDirection)
+        {
+            var targetRotation = Quaternion.LookRotation(adjustedDirection);
+            avatar.rotation = Quaternion.RotateTowards(avatar.rotation, targetRotation, turnSpeed);
+        }
+
+        private void HandleHorizontalMovement(Vector3 adjustedDirection)
+        {
+            var velocity = adjustedDirection * moveSpeed;
+            rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+        }
+
+        private void SmoothSpeed(float value)
+        {
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, value, ref velocity, smoothTime);
+        }
+    }
+}
