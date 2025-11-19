0abfad969d91cf760137c2e48fb7bb590ef3e2d0
Marc T <marc.tremblay1995@gmail.com>
2025-11-19 17:03:11 -0500

    Added ComboDefinition scriptable object

---
+++ Assets/_Project/Scripts/AbilitySystem/Ability/Definition/ComboDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityDemoA
+{
+    [CreateAssetMenu(fileName = "New Combo", menuName = "Abilities/Combo")]
+    public class ComboDefinition : ScriptableObject
+    {
+        [Tooltip( "Seconds to wait between completing a cast and resetting the combo index." )]
+        public float comboResetTime = 0.2f;
+        [Tooltip( "Seconds to wait after completing the combo before restarting the combo." )]
+        public float comboCooldownTime = 0.5f;
+        
+        public List<AbilityDefinition> comboAbilityList;
+    }
+}
